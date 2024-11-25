565557a798a29ebf5d20b5d1b6ab4c1be4159569
Matéo Avventuriero <mateo.aven@orange.fr>
2024-11-25 18:05:01 +0100

    feat: add scriptable

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialog", menuName = "Dialog/Answer")]
+public class Answer : ScriptableObject
+{
+    public Dialog dialog;
+}
---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Dialog", menuName = "Dialog/Dialogs")]
+public class Dialog : ScriptableObject
+{
+    public string text;
+    public List<Answer> answers;
+}
