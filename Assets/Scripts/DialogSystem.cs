3f84c26a543f67888c9fe9929262fecd21c2c459
Matéo Avventuriero <mateo.aven@orange.fr>
2024-12-09 19:07:41 +0100

    feat(dialog-system): add basic dialog system logic

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -1,21 +1,49 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class DialogSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private List<GameObject> answersZones;
+    [SerializeField] private Dialog currentDialog;
+    
+    private TextMeshProUGUI dialogZone;
+    
+    // Start is called before the first frame update
     void Start()
     {
-        
+        dialogZone = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("SIZE " + answersZones.Count);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<TextMeshProUGUI>().text = "Hello, World!";
+            dialogZone.text = currentDialog.text; // Display the dialog text
+            
+            int i = 0;
+            int size = currentDialog.answers.Count;
+            
+            // Check the 4 answer zones
+            foreach (GameObject answerZone in answersZones)
+            {
+                // If the dialog has answers, display them
+                if (i < size)
+                {
+                    answerZone.SetActive(true);
+                    answerZone.GetComponent<TextMeshProUGUI>().text = currentDialog.answers[i].text;
+                }
+                // Else, hide the answer zone
+                else
+                {
+                    answerZone.SetActive(false);
+                }
+
+                i++;
+            }
         }
     }
 }
